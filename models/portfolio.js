07a5693982b838d6359678b5cc353a0660338a09
auztin410 <auztin410@gmail.com>
2018-08-04 19:51:48 -0400

    Boiler plate code completed and sequelize schema structure.

---
+++ models/portfolio.js
@@ -0,0 +1,71 @@
+module.exports = function(sequelize, DataTypes){
+    var Portfolio = sequelize.define("Portfolio", {
+        name: {
+            type: DataTypes.STRING,
+            validate: {
+                allowNull: false,
+            }
+        },
+        html_css: {
+            type: DataTypes.BOOLEAN,
+            validate: {
+                allowNull: false,
+            }
+        },
+        bootstrap: {
+            type: DataTypes.BOOLEAN,
+            validate: {
+                allowNull: false,
+            }
+        },
+        materialize: {
+            type: DataTypes.BOOLEAN,
+            validate: {
+                allowNull: false,
+            }
+        },
+        css_grid: {
+            type: DataTypes.BOOLEAN,
+            validate: {
+                allowNull: false,
+            }
+        },
+        javascript: {
+            type: DataTypes.BOOLEAN,
+            validate: {
+                allowNull: false,
+            }
+        },
+        node_js: {
+            type: DataTypes.BOOLEAN,
+            validate: {
+                allowNull: false,
+            }
+        },
+        sql: {
+            type: DataTypes.BOOLEAN,
+            validate: {
+                allowNull: false,
+            }
+        },
+        mongo: {
+            type: DataTypes.BOOLEAN,
+            validate: {
+                allowNull: false,
+            }
+        },
+        react: {
+            type: DataTypes.BOOLEAN,
+            validate: {
+                allowNull: false,
+            }
+        },
+        image: {
+            type: DataTypes.STRING,
+            validate: {
+                allowNull: false,
+            }
+        }
+    });
+    return Portfolio;
+}
